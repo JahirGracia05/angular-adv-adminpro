2d42f01c7691d3b6c173ef6432a8d320a1125735
Jahir Gracia <jahirgracia05@gmail.com>
2024-01-22 18:44:57 -0700

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styles: ``
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
+  
+  public titulo: string | undefined;
+  public tituloSubs$: Subscription
+
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentosRuta()
+                        .subscribe(({titulo}) => {
+                          this.titulo = titulo;
+                          document.title = `AdminPro - ${titulo}`;
+                        });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta() {
+    return this.router.events
+      .pipe(
+        filter((event: any) => event instanceof ActivationEnd), // Filtramos sólo los de tipo ActivationEnd
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null), // Filtramos sólo el ActivationEnd donde en la pripiedad .snapshot.firstChild es null
+        map((event: ActivationEnd) => event.snapshot.data),
+      );
+    //   .subscribe(data => {
+    //     this.titulo = data['titulo'];
+    // });
+    
+  }
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -12,7 +12,7 @@ export class SidebarComponent {
 
   constructor(private sidebarService: SidebarService) {
     this.menuItems = sidebarService.menu;
-    console.log(this.menuItems);
+    // console.log(this.menuItems);
     
   }
 
